1052a74513370859410878d3d7355ca67359fc6d
Emanuel-5 <emanuel.morales5@upr.edu>
2025-12-17 01:33:51 -0400

    WinScreen

---
+++ Assets/Scripts/WinScreenManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenManager : MonoBehaviour
+{
+    [SerializeField] Button playAgainButton;
+
+    void Start()
+    {
+        playAgainButton.onClick.AddListener(RestartGame);
+    }
+    
+    void RestartGame()
+    {
+        SceneManager.LoadScene("ForestLevel");  // Carga tu escena principal (índice 0)
+    }
+}
